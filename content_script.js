454478ec7906109d73ece9f3701bf659a5ffdfe4
sandhi12 <32591427+sandhi12@users.noreply.github.com>
2019-02-17 12:34:21 +0530

    Add files via upload

---
+++ content_script.js
@@ -0,0 +1,41 @@
+chrome.runtime.onMessage.addListener(
+  function(request, sender, updateIcon) {
+    if (request.extensionStorage) {
+      // Here you specify your new font
+      var newFont = request.extensionStorage.replacementFont;
+
+      // Wrap the changer as a function
+      var changeFont = function () {
+        // Here be dragons. Lots of DOM traversal
+        var els = document.querySelectorAll('body, body *');
+
+        for (var i=0;i<els.length;i++) {
+          var oldStyle = window.getComputedStyle(els[i]).fontFamily;
+          if ([0,1].indexOf(oldStyle.indexOf(newFont)) === -1) {
+            // newFont is not the first font, add it
+            els[i].style.fontFamily = "'" + newFont + "', " + oldStyle;
+          }
+        }
+      };
+
+      // Set up the MutationObserver
+      var target = document.querySelectorAll('body')[0];
+
+      var observer, callback;
+
+      callback = function(changes) {
+        changes.forEach(function(change) {
+          changeFont();
+        });
+      };
+
+      observer = new MutationObserver(callback);
+
+      observer.observe(target, { attributes: true, childList: true});
+
+      // And then call changeFont for good measure
+      changeFont();
+      updateIcon();
+    }
+  }
+);
---
+++ popup.js
@@ -0,0 +1,53 @@
+(function () {
+    'use strict';
+    var defaultReplacementFont = "ComicSansMS";
+    var optionsForm = document.querySelectorAll('#form')[0];
+    var replacementFontInput = document.querySelectorAll('#replacementFont')[0];
+  
+    var updateFonts = function () {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(
+          tabs[0].id,
+          {
+            extensionStorage: localStorage
+          },
+          function(response) {
+            chrome.pageAction.setIcon(
+              {
+                tabId: tabs[0].id,
+                path: 'icon_enabled.png'
+              }
+            );
+          }
+        );
+      });
+    };
+  
+    var saveOptions = function () {
+      var replacementFont = replacementFontInput.value || defaultReplacementFont;
+  
+      localStorage.replacementFont = replacementFont;
+  
+      updateFonts();
+    };
+  
+    var loadOptions = function () {
+      var replacementFont = localStorage.replacementFont || defaultReplacementFont;
+  
+      replacementFontInput.value = replacementFont;
+    };
+  
+    var setupBinds = function () {
+      optionsForm.addEventListener('submit', function () {
+        saveOptions();
+        return false;
+      });
+    };
+  
+    var init = function () {
+      loadOptions();
+      setupBinds();
+    };
+  
+    init();
+  }());
